cf6ed327da9aee2cab151e1db48b03ce082a148d
Kirill Sadovnikov <d4rkm3z@gmail.com>
2021-07-24 23:37:06 +0300

    feat: set plop config to generation scss

--- plopfile.js
+++ plopfile.js
@@ -28,7 +28,7 @@ module.exports = function (plop) {
       },
       {
         type: 'add',
-        path: 'src/{{directory}}/{{pascalCase name}}/{{pascalCase name}}.module.css',
+        path: 'src/{{directory}}/{{pascalCase name}}/{{pascalCase name}}.module.scss',
         templateFile: `${componentTemplatesPath}/module.css.hbs`,
         abortOnFail: true,
       },
